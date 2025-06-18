58b3ecc10186dc4419351db14ae848846e00b205
punit1808 <punityadav1808@gmail.com>
2025-06-18 15:20:00 +0530

    default withcredentials true

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
